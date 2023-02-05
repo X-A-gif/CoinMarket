67abeb9293a2e7d6bea697041557674b3719c5a7
enamorada <109630476+AlmaHakba@users.noreply.github.com>
2023-02-05 14:42:36 -0800

    display top 20 names

--- main.js
+++ main.js
@@ -4,18 +4,22 @@ function price() {
   fetch('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        // const ethEl = document.getElementById("Eth");
-        // if(ethEl) {
-        //   ethEl.innerHTML = `1 ETH = ${data.USD} USD`;
-        // }
-    })
-    .catch(error => console.log(error));
+      console.log(data);
+      const coinData = data.Data;
+      const coinID = document.getElementsByClassName("coinID");
+      const coinPrice = document.getElementsByClassName("coinPrice");
+      for (let i = 0; i < coinID.length; i++) {
+        coinID[i].innerHTML = `${coinData[i].CoinInfo.Name}`;
+        // coinPrice[i].innerHTML =  `${coinData[i].RAW.USD.PRICE}`;
+       
+      }
+  })
+  .catch(error => console.log(error));
 }
-setInterval(price, 5000);
+setInterval(price, 1000);
 
 
 
-document.getElementById("ethBtn").addEventListener("click", function() {
+document.getElementsByClassName("coinBtn").addEventListener("click", function() {
     window.location.href = "./assets/ethChart.html";
   });
