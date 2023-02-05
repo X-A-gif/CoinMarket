dc00746998e4a5d9083b61ea9d3b1eb298106b2d
X-A-gif <109630476+X-A-gif@users.noreply.github.com>
2023-02-05 14:44:31 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 
 
 function price() {
-  fetch('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD')
+ fetch('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        // const ethEl = document.getElementById("Eth");
-        // if(ethEl) {
-        //   ethEl.innerHTML = `1 ETH = ${data.USD} USD`;
-        // }
+        const ethEl = document.getElementById("Eth");
+        if(ethEl) {
+          ethEl.innerHTML = `1 ETH = ${data.USD} USD`;
+        }
     })
     .catch(error => console.log(error));
 }
