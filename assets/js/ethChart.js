46ac106047ffc106cd5821dcd14e9f85a8b1a2f9
enamorada <109630476+AlmaHakba@users.noreply.github.com>
2023-02-11 11:42:20 -0800

    cleanded up code

--- assets/js/ethChart.js
+++ assets/js/ethChart.js
@@ -1,43 +1,4 @@
 
-// const ctx = document.getElementById('eth-chart').getContext('2d');
-// const chart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ['Red', 'Green',],
-//         datasets: [{
-//             label: 'Ethereum Price (USD)',
-//             backgroundColor: 'rgb(255, 99, 132)',
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 99, 132, 0.2)',
-//             ],
-//             data: [] 
-//         }],
-//         borderColor: [
-//             'rgba(255, 99, 132, 1)',
-//             'rgba(54, 162, 235, 1)',
-//             'rgba(255, 206, 86, 1)',
-//             'rgba(75, 192, 192, 1)',
-//             'rgba(153, 102, 255, 1)',
-//             'rgba(255, 159, 64, 1)'
-//         ],
-        
-//     },
-    
-//     pan: {
-//         enabled: true,
-//         mode: 'x'
-//     },
-//     zoom: {
-//         enabled: true,
-//         mode: 'x'
-//                 }
-// });
-
-
 function displayCoin() {
     var storedCoin = localStorage.getItem("coin");
     if (storedCoin) {
@@ -57,22 +18,6 @@ function displayCoin() {
   displayCoin();
 
 
-// const updatePrice = async () => {
-//     try {
-//         const oneWeekAgo = new Date();
-//         oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-//         const response = await fetch(`https://min-api.cryptocompare.com/data/v2/histoday?fsym=" + variable + "&tsym=USD&limit=7&toTs={Math.floor(oneWeekAgo / 1000)}`);
-//         const data = await response.json();
-//         console.log(data);
-//         chart.data.labels = data.Data.Data.map(day => new Date(day.time * 1000).toLocaleDateString());
-//         chart.data.datasets[0].data = data.Data.Data.map(day => day.close);
-//         chart.update();
-//     } catch (error) {
-//         console.error(error);
-//     }
-// };
-
-// updatePrice();
 
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(getData);
@@ -81,10 +26,7 @@ google.charts.setOnLoadCallback(getData);
 function getData(){
   var coin = JSON.parse(localStorage.getItem("coin"));
   var symbol = coin.symbol;
-   // var symbol = "ETH"
-    //var histDataUrl = "https://min-api.cryptocompare.com/data/v2/histoday?fsym=" + symbol + "&tsym=USD&limit=30&aggregate=3&e=CCCAGG"
-    
-    //var histDataUrl = "https://min-api.cryptocompare.com/data/v2/histoday?fsym=ETH&tsym=USD&limit=30&aggregate=3&e=CCCAGG"
+   
     var histDataUrl = "https://min-api.cryptocompare.com/data/v2/histoday?fsym=" + symbol + "&tsym=USD&limit=4"
 
     fetch(histDataUrl)
@@ -121,7 +63,7 @@ function getData(){
     
     var options = {
         legend: 'none',
-        //bar: { groupWidth: '100%' }, // Remove space between bars.
+
         candlestick: {
           fallingColor: { strokeWidth: 0, fill: '#a52714' }, // red
           risingColor: { strokeWidth: 0, fill: '#0f9d58' }   // green
--- assets/js/news.js
+++ assets/js/news.js
@@ -26,26 +26,32 @@ function searchApi(query){
               var divCardImageEl = document.createElement("div");
               var divCardContentEl = document.createElement("div");
               var divCardActionEl = document.createElement("div");
+
               divCardImageEl.setAttribute("class","card-image small");
               divCardContentEl.setAttribute("class", "card-content");
               divCardActionEl.setAttribute("class", "card-action")
+
               var imageEl = document.createElement("img");
               var headlineEl = document.createElement("h5");
               var descriptionEl = document.createElement("p");
               var linkEl = document.createElement("a");
+
               linkEl.setAttribute("target", "_blank")
              // if(data.Data[i].urlToImage != null){
               imageEl.src = data.Data[i].imageurl;
             //  }
               headlineEl.textContent = data.Data[i].title;
               descriptionEl.textContent = data.Data[i].body;
+
               linkEl.href = data.Data[i].url;
               linkEl.textContent = "Read More"
               cardEl[0].appendChild(divCardImageEl);
               divCardImageEl.appendChild(imageEl);
+
               cardEl[0].appendChild(divCardContentEl);
               divCardContentEl.appendChild(headlineEl);
               divCardContentEl.appendChild(descriptionEl);
+              
               cardEl[0].appendChild(divCardActionEl);
               divCardActionEl.appendChild(linkEl)
         }
