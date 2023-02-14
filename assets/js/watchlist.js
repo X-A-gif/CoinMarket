9a0ada9b45e9d22fd1216b9eb0cc1916af0235ce
Haley T <hntrimpert@yahoo.com>
2023-02-13 19:55:11 -0500

    bug fix

--- assets/js/watchlist.js
+++ assets/js/watchlist.js
@@ -7,8 +7,10 @@ function displayCoin() {
     
     let list = document.createElement("ul");
     for(let i=0; i < coinObj.length; i++) {
+        console.log(coinObj[i].name);
         let item = document.createElement("li");
-        item.innerHTML = coinObj[i];
+        item.innerHTML = coinObj[i].name;
+
         list.appendChild(item);
     }
     document.body.appendChild(list);
