f58856f6437f5aad4630bd21efc68782719fdfb0
enamorada <109630476+AlmaHakba@users.noreply.github.com>
2023-02-14 13:12:22 -0800

    last changes

--- assets/js/watchlist.js
+++ assets/js/watchlist.js
@@ -1,20 +1,69 @@
-function displayCoin() {
-    var storedCoin = localStorage.getItem("allFavorites");
-    if (storedCoin) {
-      var coinObj = JSON.parse(storedCoin);
-      console.log(coinObj);
-      var coinName = coinObj.name;
-    
-    let list = document.createElement("ul");
-    for(let i=0; i < coinObj.length; i++) {
-        console.log(coinObj[i].name);
-        let item = document.createElement("li");
-        item.innerHTML = coinObj[i].name;
+function price() { 
+  var tableBody = document.querySelector("tbody");
 
-        list.appendChild(item);
+  var storedCoin = localStorage.getItem("allFavorites");
+  if (storedCoin) {
+    var coinObj = JSON.parse(storedCoin);
+    console.log(coinObj);
+ 
+      for(let i=0; i < coinObj.length; i++) {
+         
+         var tableRow = document.createElement("tr");
+ 
+         var tableIndex = document.createElement("td");
+         var tableCoinName = document.createElement("td");
+         var tableNameAnchor = document.createElement("td");
+ 
+         var tableCoinPrice = document.createElement("td");
+         var tableHour = document.createElement("td");
+         var tableMktCap = document.createElement("td");
+
+          
+         var fullNameAtt = document.createAttribute("data-fn");
+         var priceAtt = document.createAttribute("data-price");
+         var marketAtt = document.createAttribute("data-mktCap");
+         var coinSupply = document.createAttribute("data-supply");
+         var coinSupply = document.createAttribute("data-vovl");
+ 
+         var classAtt = document.createAttribute("class");
+ 
+         tableIndex.textContent = i+1;
+         tableRow.appendChild(tableIndex);
+          
+         tableNameAnchor.innerHTML = coinObj[i].name
+   
+           
+         
+         tableNameAnchor.setAttributeNode(fullNameAtt);
+         tableNameAnchor.setAttributeNode(priceAtt);
+         tableNameAnchor.setAttributeNode(marketAtt);
+         tableNameAnchor.setAttributeNode(coinSupply);
+
+         tableNameAnchor.setAttributeNode(classAtt);
+         tableNameAnchor.setAttribute("class", "coinLink");
+         tableCoinName.appendChild(tableNameAnchor);
+         tableRow.appendChild(tableCoinName);
+        
+ 
+         tableCoinPrice.innerHTML = coinObj[i].price
+         tableRow.appendChild(tableCoinPrice);
+ 
+         
+         tableHour.innerHTML = coinObj[i].supply
+         tableRow.appendChild(tableHour);
+         
+         tableRow.appendChild(tableMktCap);
+         tableMktCap.innerHTML = coinObj[i].marketCap
+ 
+   
+ 
+
+         tableBody.appendChild(tableRow);
+
+        
+       }
+      }
     }
-    document.body.appendChild(list);
-}
-  }
-
-  displayCoin();
+ 
+ 
+ price();
